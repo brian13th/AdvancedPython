f893518ece7465689ec5f41c2e72f60b50c6e5f9
brian13th <brian13th13@gmail.com>
2019-04-24 04:09:09 +0300

    add feature commands insert and delete points

--- week2/point.py
+++ week2/point.py
@@ -14,11 +14,28 @@ class Point():
 
 
 while True:
-    coords = input('Δώστε συντεταγμένες σημείου (χ,y): ')
-    if coords == '': break
+    command = input('Δώστε εντολή insert x,y ή delete x,y: ')
+    if command == '' : break
+    if len(command.split())<2 : continue
+    coords = command.split()[1]
     x,y = coords.split(',')
     if x.isdigit() and y.isdigit():
-        new_point = Point(x,y)
-        for p in Point.points:
-            if p != new_point:
-                print(f"Η απόσταση για το σημείο {p} είναι: {p.distance(new_point)}")
+        if command.split()[0] == 'insert':
+            new_point = Point(x,y)
+            print(f" Υπάρχουν συνολικά {len(Point.points)} σημεία")
+            for p in Point.points:
+                if p != new_point:
+                    print(f"Το σημείο {p} είναι σε απόσταση: {p.distance(new_point)} από το σημείο")
+        elif command.split()[0] == 'delete':
+            deleted = False
+            new_points = []
+            for p in Point.points:
+                if p.x == int(x) and p.y == int(y):
+                    del p
+                    deleted = True
+                else: new_points.append(p)
+            Point.points = new_points
+            if deleted:
+                print(f"Τα εναπομείναντα σημεία είναι:")
+                for p in Point.points: print(p)
+            else: print(f"Το σημείο δεν βρέθηκε")
