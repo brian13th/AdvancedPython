4b3e3de3de77a82b3fe92b18467689e028e8300d
brian13th <brian13th13@gmail.com>
2019-05-07 03:46:09 +0300

    new version of contacts with the ability to store the contacts permanently to disc as csv file

---
+++ week3/contacts_v1.py
@@ -0,0 +1,79 @@
+import os, random, csv
+
+class Contact():
+    """κάθε επαφή είναι όνομα και τηλέφωνο"""
+    theContacts = {}
+    def __init__(self, name, number):
+        self.name = name.strip()
+        self.number = number.strip()
+        Contact.theContacts[self.name]=self
+    def __repr__(self):
+        return self.name + ' : ' + self.number
+    def list_contacts():
+        for c in sorted(Contact.theContacts):
+            print(Contact.theContacts[c])
+
+class Persistant():
+    """κλάση μόνιμης αποθήκευσης των αντικειμένων της Contact"""
+    def __init__(self):
+        self.filename = 'contacts.csv'
+    def store(self):
+        with open(self.filename, 'wt', encoding='utf-8') as f:
+            writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_ALL)
+            for c, contact in Contact.theContacts.items():
+                writer.writerow([contact.name, contact.number])
+    def retrive(self):
+        if os.path.isfile(self.filename):
+            with open(self.filename, 'rt', encoding='utf-8') as f:
+                reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_ALL)
+                for row in reader:
+                    Contact(row[0], row[1])
+                    return True
+        else: return True
+
+class Main():
+    """κλάση διαχείρισης επαφών - δημιουργία - διαγραφή"""
+    def __init__(self):
+        persist = Persistant()
+        persist.retrive()
+        while True:
+            command = input(f'\nΕπαφές {len(Contact.theContacts)}\nΕισαγωγή[+] Διαγραφή[-] Επισκόπηση[?] Έξοδος[Enter]')
+            if command == '': break
+            elif command == "+":
+                contact_details = input('Εισάγετε όνομα : τηλέφωνο ή πλήθος επαφών αυτόματης εισαγωγής ')
+                if ':' in contact_details:
+                    try: Contact(*contact_details.split(':'))
+                    except IndexError: print('Σφάλμα εισαγωγής επαφής')
+                elif contact_details.isdigit():
+                    if int(contact_details) < 500:
+                        self.create_contacts(int(contact_details))
+                    else:  print('Σφάλμα: Δώσατε πολύ μεγάλο αριθμό επαφών..')
+                else: print('Προσοχή! Δώστε όνομα (:) τηλέφωνο')
+            elif command == "?":
+                Contact.list_contacts()
+            elif command == "-":
+                name = input('Διαγραφή όνομα ')
+                try:
+                    del Contact.theContacts[name.strip()]
+                except KeyError:
+                    print('Δεν υπάρχει η επαφή που ζητήσατε')
+        persist.store()
+    def create_contacts(self, size):
+        """Δημιουργεί τυχαίο δείγμα επαφών και τηλεφώνων"""
+        dir = './data'
+        act_names_files = [os.path.join(dir,x) for x in ('gr_actresses.txt', 'gr_actors.txt')]
+        names = []
+        for f in act_names_files:
+            with open(f, 'r', encoding='utf-8') as fin:
+                for name in fin:
+                    if len(name) > 2:
+                        if len(name.split()) > 1:
+                            names.append(name.strip())
+        if size < len(names):
+            contact_names = random.sample(names, size)
+        else:
+            contact_names = names
+        for contact in contact_names:
+            Contact(contact, str(random.randint(6900000000, 6999999999)))
+
+if __name__ == '__main__': Main()
